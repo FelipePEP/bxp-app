cf7048233695d681f4bb1a06d1999ae4bf1bb04b
felipe leandro <foliveira.webdev@gmail.com>
2022-09-27 16:06:01 -0300

    refactor: alter some imports

--- src/data/usecases/generate-weapon/remote-generate-weapon.spec.ts
+++ src/data/usecases/generate-weapon/remote-generate-weapon.spec.ts
@@ -1,17 +1,10 @@
-import { HttpPostClient } from '../../../infra/protocols/http/http-post-client'
+import { HttpPostClientSpy } from '../../test/mock-http-client'
 import { RemoteGenerateWeapon } from './remote-generate-weapon'
 
 describe('Generate random wapon', () => {
   test('Should call correct URL with HttpPostClient', async () => {
-    class HttpClientSpy implements HttpPostClient {
-        url?: string
-        async post (url: string): Promise<void> {
-          this.url = url
-          return Promise.resolve()
-        }
-    }
     const url = 'xpto'
-    const httpPostClientSpy = new HttpClientSpy()
+    const httpPostClientSpy = new HttpPostClientSpy()
     const sut = new RemoteGenerateWeapon(url, httpPostClientSpy)
     await sut.generateWeapon()
     expect(httpPostClientSpy.url).toBe(url)
--- src/data/usecases/generate-weapon/remote-generate-weapon.ts
+++ src/data/usecases/generate-weapon/remote-generate-weapon.ts
@@ -1,4 +1,4 @@
-import { HttpPostClient } from '../../../infra/protocols/http/http-post-client'
+import { HttpPostClient } from '../../protocols/http/http-post-client'
 
 export class RemoteGenerateWeapon {
   constructor (
