3a2e7d50bf988ee994bceb58569f5952799307b6
felipe leandro <foliveira.webdev@gmail.com>
2022-09-27 16:31:44 -0300

    refactor: add sut factory method

--- src/data/usecases/generate-weapon/remote-generate-weapon.spec.ts
+++ src/data/usecases/generate-weapon/remote-generate-weapon.spec.ts
@@ -1,11 +1,21 @@
 import { HttpPostClientSpy } from '../../test/mock-http-client'
 import { RemoteGenerateWeapon } from './remote-generate-weapon'
 
+type SutTypes = {
+  sut: RemoteGenerateWeapon
+  httpPostClientSpy: HttpPostClientSpy
+}
+
+const makeSut = (url: string = 'foo'): SutTypes =>{
+  const httpPostClientSpy = new HttpPostClientSpy()
+  const sut = new RemoteGenerateWeapon(url, httpPostClientSpy)
+  return { sut, httpPostClientSpy }
+}
+
 describe('Generate random wapon', () => {
   test('Should call correct URL with HttpPostClient', async () => {
-    const url = 'xpto'
-    const httpPostClientSpy = new HttpPostClientSpy()
-    const sut = new RemoteGenerateWeapon(url, httpPostClientSpy)
+    const url = 'bar'
+    const { sut, httpPostClientSpy } = makeSut(url)
     await sut.generateWeapon()
     expect(httpPostClientSpy.url).toBe(url)
   })
