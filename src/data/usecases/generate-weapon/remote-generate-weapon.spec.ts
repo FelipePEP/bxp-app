0f9cea4ec2904c384040c923dc0c8e2e2a0d95d6
felipe leandro <foliveira.webdev@gmail.com>
2022-10-01 17:25:25 -0300

    test: add invalid params error test

--- src/data/usecases/generate-weapon/remote-generate-weapon.spec.ts
+++ src/data/usecases/generate-weapon/remote-generate-weapon.spec.ts
@@ -1,14 +1,18 @@
 import { HttpPostClientSpy } from '@/data/mock/mock-http-client'
+import { HttpStatusCode } from '@/data/protocols/http/http-response'
 import { RemoteGenerateWeapon } from '@/data/usecases/generate-weapon/remote-generate-weapon'
+import { InvalidParamsError } from '@/domain/error/invalid-params'
+import { WeaponModel } from '@/domain/models/weapon-model'
+import { GenerateWeapon } from '@/domain/usecases/generate-weapon'
 import { faker } from '@faker-js/faker'
 
 type SutTypes = {
   sut: RemoteGenerateWeapon
-  httpPostClientSpy: HttpPostClientSpy
+  httpPostClientSpy: HttpPostClientSpy<GenerateWeapon.params, WeaponModel>
 }
 
 const makeSut = (params: any = { url: faker.internet.url() }): SutTypes => {
-  const httpPostClientSpy = new HttpPostClientSpy()
+  const httpPostClientSpy = new HttpPostClientSpy<GenerateWeapon.params, WeaponModel>()
   const sut = new RemoteGenerateWeapon(params.url, httpPostClientSpy)
   return { sut, httpPostClientSpy }
 }
@@ -16,7 +20,7 @@ const makeSut = (params: any = { url: faker.internet.url() }): SutTypes => {
 describe('Generate random weapon', () => {
   test('Should call correct URL with HttpPostClient', async () => {
     const url = faker.internet.url()
-    const { sut, httpPostClientSpy } = makeSut({url: url})
+    const { sut, httpPostClientSpy } = makeSut({ url: url })
     await sut.randomize(null)
     expect(httpPostClientSpy.url).toBe(url)
   })
@@ -26,6 +30,14 @@ describe('Generate random weapon', () => {
     const { sut, httpPostClientSpy } = makeSut()
     await sut.randomize(body)
     expect(httpPostClientSpy.body).toBe(body)
+  }
+  )
+  test('Should throw InvalidParamsError if HttpPostClient returns 400', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.badRequest
+    }
+    const promise = sut.randomize(null)
+    await expect(promise).rejects.toThrow(new InvalidParamsError())
   })
 })
-
