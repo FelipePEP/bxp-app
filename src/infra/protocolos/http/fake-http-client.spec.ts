41a836bb65ae0c1fc7937b1fdfede001a2d3bc36
felipe leandro <foliveira.webdev@gmail.com>
2022-10-01 17:40:28 -0300

    feat: add fake-http-client in infra layer

---
+++ src/infra/protocolos/http/fake-http-client.spec.ts
@@ -0,0 +1,5 @@
+describe('FakeHttpClient', () => {
+    test('',()=>{
+        
+    })
+})
--- src/infra/protocolos/http/fake-http-client.ts
+++ src/infra/protocolos/http/fake-http-client.ts
@@ -1,7 +1,11 @@
 import { HttpPostClient, HttpPostParams } from '@/data/protocols/http/http-post-client'
+import { HttpResponse } from '@/data/protocols/http/http-response'
 
-export class FakeHttpClient implements HttpPostClient {
-  async post (params: HttpPostParams): Promise<void> {
-    return Promise.resolve()
+export class FakeHttpClient<T, R> implements HttpPostClient<T, R> {
+  async post (params: HttpPostParams<T>): Promise<HttpResponse<R>> {
+    return Promise.resolve({
+      statusCode: 200,
+      body: null
+    })
   }
 }
